2e35cfdaa10c7bdfbe7b83a6279ae7bf9138ed7b
Njooro <135953380+Njooro@users.noreply.github.com>
2024-02-10 07:01:03 +0300

    Update Patient.js

--- client/src/components/Patient.js
+++ client/src/components/Patient.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 const Patient = ( ) => {
     const [patients, setPatients] = useState([])
-
+    
     useEffect(() => {
         fetch('/patients')
         .then(response => response.json())
